7db623d8668a3dc8fd7d3152bdaaf3ea7bea26a1
Pedro La Rosa <luigi5021@gmail.com>
2016-12-07 23:51:36 -0400

    Finalize tests & add page title

--- app/components/Countdown.js
+++ app/components/Countdown.js
@@ -92,6 +92,7 @@ class Countdown extends Component {
     };
     return (
       <div>
+        <h1 className="page-title">Countdown App</h1>
         <Clock totalSeconds={count} />
         {renderControlArea()}
       </div>
